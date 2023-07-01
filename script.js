4e0bfa3301009b4fbb7d8048d43427a7f69909b0
ma-boles <120141149+ma-boles@users.noreply.github.com>
2023-06-30 18:24:53 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+//displays answers
+//changes direction of arrow img 
+
+function toggle1() {
+     var text = document.getElementById("answerField1");
+     if(text.style.display === "none") {
+        text.style.display = "block"
+     } else {
+        text.style.display = "none"
+     }
+
+    var arrow = document.getElementById("arrow");
+    if(arrow.style.transform === "rotate(180deg)") {
+        arrow.style.transform = "none"
+    } else {
+        arrow.style.transform = "rotate(180deg)"
+    }
+      
+}
+ 
+function toggle2() {
+    var text = document.getElementById("answerField2");
+     if(text.style.display === "none") {
+        text.style.display = "block"
+     } else {
+        text.style.display = "none"
+     }
+
+     var arrow = document.getElementById("arrow2");
+     if(arrow.style.transform === "rotate(180deg)") {
+         arrow.style.transform = "none"
+     } else {
+         arrow.style.transform = "rotate(180deg)"
+     }
+
+}
+function toggle3() {
+    var text = document.getElementById("answerField3");
+     if(text.style.display === "none") {
+        text.style.display = "block"
+     } else {
+        text.style.display = "none"
+     }
+
+    var arrow = document.getElementById("arrow3");
+    if(arrow.style.transform === "rotate(180deg)") {
+        arrow.style.transform = "none"
+    } else {
+        arrow.style.transform = "rotate(180deg)"
+    }
+
+}
+function toggle4() {
+    var text = document.getElementById("answerField4");
+     if(text.style.display === "none") {
+        text.style.display = "block"
+     } else {
+        text.style.display = "none"
+     }
+
+     var arrow = document.getElementById("arrow4");
+     if(arrow.style.transform === "rotate(180deg)") {
+         arrow.style.transform = "none"
+     } else {
+         arrow.style.transform = "rotate(180deg)"
+     }
+
+}
+function toggle5() {
+    var text = document.getElementById("answerField5");
+     if(text.style.display === "none") {
+        text.style.display = "block"
+     } else {
+        text.style.display = "none"
+     }
+
+     var arrow = document.getElementById("arrow5");
+     if(arrow.style.transform === "rotate(180deg)") {
+         arrow.style.transform = "none"
+     } else {
+         arrow.style.transform = "rotate(180deg)"
+     }
+
+}
+       
