b0711c8920dfe76129a477f0f6b803a7df7a45ae
ma-boles <120141149+ma-boles@users.noreply.github.com>
2023-07-03 14:08:25 -0700

    Update script.js
    added bold function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //displays answers
 //changes direction of arrow img 
+//questions become bold 
 
 function toggle1() {
      var text = document.getElementById("answerField1");
@@ -15,7 +16,13 @@ function toggle1() {
     } else {
         arrow.style.transform = "rotate(180deg)"
     }
-      
+
+    var text = document.getElementById("question1");
+    if (text.style.fontWeight === "bold") {
+        text.style.fontWeight = "normal"
+    } else {
+        text.style.fontWeight = "bold"
+    }
 }
  
 function toggle2() {
@@ -33,7 +40,14 @@ function toggle2() {
          arrow.style.transform = "rotate(180deg)"
      }
 
+     var text = document.getElementById("question2");
+     if (text.style.fontWeight === "bold") {
+         text.style.fontWeight = "normal"
+     } else {
+         text.style.fontWeight = "bold"
+     }
 }
+
 function toggle3() {
     var text = document.getElementById("answerField3");
      if(text.style.display === "none") {
@@ -49,7 +63,14 @@ function toggle3() {
         arrow.style.transform = "rotate(180deg)"
     }
 
+    var text = document.getElementById("question3");
+    if (text.style.fontWeight === "bold") {
+        text.style.fontWeight = "normal"
+    } else {
+        text.style.fontWeight = "bold"
+    }
 }
+
 function toggle4() {
     var text = document.getElementById("answerField4");
      if(text.style.display === "none") {
@@ -64,8 +85,15 @@ function toggle4() {
      } else {
          arrow.style.transform = "rotate(180deg)"
      }
-
+     
+     var text = document.getElementById("question4");
+     if (text.style.fontWeight === "bold") {
+         text.style.fontWeight = "normal"
+     } else {
+         text.style.fontWeight = "bold"
+     }
 }
+
 function toggle5() {
     var text = document.getElementById("answerField5");
      if(text.style.display === "none") {
@@ -80,6 +108,12 @@ function toggle5() {
      } else {
          arrow.style.transform = "rotate(180deg)"
      }
-
+     
+     var text = document.getElementById("question5");
+     if (text.style.fontWeight === "bold") {
+         text.style.fontWeight = "normal"
+     } else {
+         text.style.fontWeight = "bold"
+     }
 }
        
